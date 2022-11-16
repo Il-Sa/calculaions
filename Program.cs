66e9b57fbeff80f228747bc1cc7c856c6e4a23dd
Ilze Safi <ilze.safi2021@gmail.com>
2022-11-16 15:36:28 +0200

    calculations

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter the first number:");
+string number1 = Console.ReadLine();
+Console.WriteLine("Enter the second number:");
+string number2 = Console.ReadLine();
+int num1 = Int32.Parse(number1);
+int num2 = Int32.Parse(number2);
+int addition = num1 + num2;
+Console.WriteLine("Addition: " + addition);
+int subtraction = num1 - num2;
+Console.WriteLine("Subtraction: " + subtraction);
+int multipliation = num1 * num2;
+Console.WriteLine("Multipliation: " + multipliation);
+double no1 = num1;
+double no2 = num2;
+double division = no1 / no2;
+Console.WriteLine("Division: " + division);
